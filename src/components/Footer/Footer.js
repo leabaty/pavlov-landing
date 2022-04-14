75dae198be659947b7d9797f63e6987948a6dbc7
Léa Baty <leabaty.dev@gmail.com>
2022-04-14 10:05:26 +0200

    Style : Simplified footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,34 +10,6 @@ function Footer() {
   return (
     <footer className="footer__container app__footer">
       <div className="footer__content">
-        <div className="social__icons">
-          <a
-            className="social__icon-link"
-            href="https://github.com/leabaty"
-            target="_blank"
-            aria-label="Github"
-          >
-            <AiFillGithub />
-          </a>
-          <a
-            className="social__icon-link"
-            href="https://www.linkedin.com/in/l%C3%A9a-baty-2a542375"
-            target="_blank"
-            aria-label="LinkedIn"
-          >
-            <AiFillLinkedin />
-          </a>
-
-          <a
-            className="social__icon-link"
-            href="mailto: leabaty.dev@gmail.com"
-            target="_blank"
-            aria-label="Email me"
-          >
-            <HiOutlineMailOpen />
-          </a>
-        </div>
-
         <div className="footer__website-rights">Léa Baty © 2022</div>
       </div>
     </footer>
