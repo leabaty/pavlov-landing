e4f36b901219aee4f8a874af38b49fffd1330f30
Léa <80676681+leabaty@users.noreply.github.com>
2022-08-23 10:55:17 +0200

    Update Human.js

--- src/components/Human/Human.js
+++ src/components/Human/Human.js
@@ -20,7 +20,7 @@ function Human({ addressDetails }) {
 
           <div className="content__inside">
     
-            {/*<p className="text">
+            <p className="text">
               J'habite au 1 rue des Rainettes, à Annecy-le-Vieux.
             </p>
 
@@ -33,9 +33,9 @@ function Human({ addressDetails }) {
                 title="1 rue des Rainettes Annecy-le-Vieux"
                 width="100%"
               ></iframe>
-            </div>*/}
+            </div>
 
-             <p className="text">J'habite au 15 Faubourg des Balmettes.</p>
+            {/* <p className="text">J'habite au 15 Faubourg des Balmettes.</p>
 
             <div className="map">
               <iframe
@@ -46,7 +46,7 @@ function Human({ addressDetails }) {
                 title="15 faubourg des balmettes Annecy"
                 width="100%"
               ></iframe>
-            </div>
+            </div>*/}
 
             {/* <p className="text">J'habite au {addressDetails.address}.</p>
 
