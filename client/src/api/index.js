e3132f2312512a11a9a290d28daa99965c81395a
Léa Baty <leabaty.dev@gmail.com>
2022-10-02 18:20:50 +0200

    Update : api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/visitorsbook';
+const url = `${process.env.REACT_APP_API_URL}/visitorsbook`;
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
